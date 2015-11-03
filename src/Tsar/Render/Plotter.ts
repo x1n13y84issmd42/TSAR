fda8f97990d1f9855094e900031cb60a454b52a1
Eugene S. Merkoulov <gaddeena@gmail.com>
2015-11-03 19:13:03 +0200

    Plotter WIP

--- src/Tsar/Render/Plotter.ts
+++ src/Tsar/Render/Plotter.ts
@@ -5,16 +5,20 @@ module Tsar.Render.Debug
 {
 	class Plot
 	{
+		public name: string;
 		public values: number[] = [];
-		private capacity: number = 0;
 		public maximum: number = 0;
+		public minimum: number = 0;
+		private capacity: number = 0;
+
 		public options = {
 			color: 'white',
 			thickness: 1
 		};
 
-		public constructor(capacity: number, options:{color:string, thickness:number})
+		public constructor(name, capacity: number, options:{color:string, thickness:number})
 		{
+			this.name = name;
 			this.capacity = capacity;
 			this.options = options;
 		}
@@ -28,8 +32,9 @@ module Tsar.Render.Debug
 				this.values.shift();
 			}
 
-			//	Finding the maximum value to create a proper Y axis
-			this.maximum = gMath.max.apply(null, this.values);
+			//	Finding the maximum & minimum values to create a proper Y axis
+			this.maximum = jMath.max.apply(null, this.values);
+			this.minimum = jMath.min.apply(null, this.values);
 		}
 	}
 
@@ -39,6 +44,7 @@ module Tsar.Render.Debug
 		private capacity: number = 0;
 		private position = new Tsar.Math.float2(0, 0);
 		private dimensions = new Tsar.Math.float2(100, 100);
+		private options = {range:[-1, 1]};
 
 		public constructor(capacity:number)
 		{
@@ -57,17 +63,26 @@ module Tsar.Render.Debug
 
 		public addChart(name: string, options: {color:string, thickness:number})
 		{
-			if (! this.charts[name])
+			for (var cI in this.charts)
 			{
-				this.charts[name] = new Plot(this.capacity, options);
+				if (this.charts[cI].name == name)
+				{
+					return;
+				}
 			}
+				
+			this.charts.push(new Plot(name, this.capacity, options));
 		}
 
 		public addValue(plotName:string, n:number)
 		{
-			if (this.charts[plotName])
+			for (var cI in this.charts)
 			{
-				this.charts[plotName].addValue(n);
+				if (this.charts[cI].name == name)
+				{
+					this.charts[cI].addValue(n);
+					return;
+				}
 			}
 		}
 
@@ -76,19 +91,54 @@ module Tsar.Render.Debug
 			C.globalCompositeOperation = "source-over";
 			C.lineWidth = 1;
 			C.lineCap = 'round';
-		//	var maximum = gMath.max.apply(null, underscore.pluck(this.charts, 'maximum'));
+			var maximum = this.findMaximum();
+			var minimum = this.findMinimum();
 			var origin = this.position;
 			var dimensions = this.dimensions;
 			var offset = new Tsar.Math.float2(20, -20);
 		//	var YLabels = this.computeYLabels(maximum);
 
-			this.renderAxes(C, origin, dimensions, offset);
+			this.renderAxes(C, origin, dimensions, offset, minimum<0);
 
 			for (var cI in this.charts)
 			{
 				var plot = this.charts[cI];
 				this.plot(plot, C, origin, dimensions, offset);
 			}
+		}
+
+		private findMaximum()
+		{
+			if (this.charts.length)
+			{
+				var maximum = this.charts[0].maximum;				
+
+				for (var cI in this.charts)
+				{
+					maximum = jMath.max(maximum, this.charts[cI].maximum);
+				}
+
+				return maximum;
+			}
+
+			return 0;
+		}
+
+		private findMinimum()
+		{
+			if (this.charts.length)
+			{
+				var minimum = this.charts[0].minimum;				
+
+				for (var cI in this.charts)
+				{
+					minimum = jMath.min(minimum, this.charts[cI].minimum);
+				}
+
+				return minimum;
+			}
+
+			return 0;
 		}
 
 		private plot(plot, C, origin, dimensions, offset)
@@ -123,7 +173,7 @@ module Tsar.Render.Debug
 						C.context.fillText(v.toFixed(3), x, y);
 						/*
 						C.beginPath();
-						C.arc(x, y, 2, 0, gMath.PI * 2);
+						C.arc(x, y, 2, 0, jMath.PI * 2);
 						C.closePath();
 						C.fill();
 						*/
@@ -135,17 +185,27 @@ module Tsar.Render.Debug
 			}
 		}
 
-		private renderAxes(C, origin, dimensions, offset)
+		private renderAxes(C, origin, dimensions, offset, negative)
 		{
 			C.strokeStyle = "red";
 			C.fillStyle = "white";
 			C.beginPath();
 
+			//	Y
 			C.moveTo(origin.x + offset.x, origin.y);
 			C.lineTo(origin.x + offset.x, origin.y + dimensions.y);
 
-			C.moveTo(origin.x, origin.y + offset.y + dimensions.y);
-			C.lineTo(origin.x + dimensions.x, origin.y + offset.y + dimensions.y);
+			//	X
+			if (negative)
+			{
+				C.moveTo(origin.x, origin.y + offset.y + dimensions.y / 2);
+				C.lineTo(origin.x + dimensions.x, origin.y + offset.y + dimensions.y / 2);				
+			}
+			else
+			{
+				C.moveTo(origin.x, origin.y + offset.y + dimensions.y);
+				C.lineTo(origin.x + dimensions.x, origin.y + offset.y + dimensions.y);				
+			}
 
 			C.closePath();
 			C.stroke();
